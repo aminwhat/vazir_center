19407572879f1f1f6dbcc253b0659dee8ad5e814
AminWhat <m.aminwhat@gmail.com>
2024-03-15 21:51:57 +0330

    seperate clients

--- switcher/src/client.rs
+++ switcher/src/client.rs
@@ -1,59 +1,83 @@
 use futures_util::future::FutureExt;
-use rust_socketio::{client::ClientBuilder, Payload, RawClient};
-use serde_json::json;
+use rust_socketio::{
+    client::{Client, ClientBuilder},
+    Event, Payload, RawClient,
+};
 use std::time::Duration;
 
-// #[tokio::main]
-async fn client() {
-    //! was main
-    // define a callback which is called when a payload is received
-    // this callback gets the payload as well as an instance of the
-    // socket to communicate with the server
-    let callback = |payload: Payload, socket: RawClient| {
-        async move {
-            match payload {
-                Payload::String(str) => println!("Received: {}", str),
-                Payload::Binary(bin_data) => println!("Received bytes: {:#?}", bin_data),
-            }
-            socket
-                .emit("test", json!({"got ack": true}))
-                .expect("Server unreachable");
+pub struct ClientSwitcher<T, F, E, D>
+where
+    T: Into<String>,
+    F: FnMut(Payload, RawClient) + 'static + Send,
+    E: Into<Event>,
+    D: Into<Payload>,
+{
+    socket: Client,
+    _t: std::marker::PhantomData<T>,
+    _f: std::marker::PhantomData<F>,
+    _e: std::marker::PhantomData<E>,
+    _d: std::marker::PhantomData<D>,
+}
+
+impl<T, F, E, D> ClientSwitcher<T, F, E, D>
+where
+    T: Into<String>,
+    F: FnMut(Payload, RawClient) + 'static + Send,
+    E: Into<Event>,
+    D: Into<Payload>,
+{
+    pub fn new(address: T, namespace: T) -> Self {
+        // get a socket that is connected to the admin namespace
+        let socket = ClientBuilder::new(address)
+            .namespace(namespace)
+            .on("error", |err, _| {
+                async move { eprintln!("Error: {:#?}", err) }.boxed();
+            })
+            .connect()
+            .expect("Connection failed");
+
+        Self {
+            socket,
+            _t: std::marker::PhantomData,
+            _e: std::marker::PhantomData,
+            _d: std::marker::PhantomData,
+            _f: std::marker::PhantomData,
         }
-        .boxed()
-    };
+    }
 
-    // get a socket that is connected to the admin namespace
-    let socket = ClientBuilder::new("http://localhost:4200/")
-        .namespace("/admin")
-        .on("test", move |payload: Payload, client: RawClient| {
-            callback(payload, client);
-        })
-        .on("error", |err, _| {
-            async move { eprintln!("Error: {:#?}", err) }.boxed();
-        })
-        .connect()
-        .expect("Connection failed");
+    pub fn new_with_error(address: T, namespace: T, on_error: F) -> Self {
+        // get a socket that is connected to the admin namespace
+        let socket = ClientBuilder::new(address)
+            .namespace(namespace)
+            .on("error", on_error)
+            .connect()
+            .expect("Connection failed");
 
-    // emit to the "foo" event
-    let json_payload = json!({"token": 123});
-    socket
-        .emit("foo", json_payload)
-        .expect("Server unreachable");
-
-    // define a callback, that's executed when the ack got acked
-    let ack_callback = |message: Payload, _: RawClient| {
-        async move {
-            println!("Yehaa! My ack got acked?");
-            println!("Ack data: {:#?}", message);
+        Self {
+            socket,
+            _t: std::marker::PhantomData,
+            _e: std::marker::PhantomData,
+            _d: std::marker::PhantomData,
+            _f: std::marker::PhantomData,
         }
-        .boxed();
-    };
+    }
 
-    let json_payload = json!({"myAckData": 123});
-    // emit with an ack
-    socket
-        .emit_with_ack("test", json_payload, Duration::from_secs(2), ack_callback)
-        .expect("Server unreachable");
+    pub fn emit(&self, event: E, data: D) -> Result<(), ()> {
+        &self.socket.emit(event, data).expect("Server unreachable");
+        Ok(())
+    }
 
-    socket.disconnect().expect("Disconnect failed");
+    pub fn emit_with_ack(&self, event: E, data: D, callback: F) -> Result<(), ()> {
+        &self
+            .socket
+            .emit_with_ack(event, data, Duration::from_secs(3), callback)
+            .expect("Server unreachable");
+
+        Ok(())
+    }
+
+    pub fn disconnect(&self) -> Result<(), ()> {
+        &self.socket.disconnect().expect("Disconnect failed");
+        Ok(())
+    }
 }
