09e477c131861c19452cbb00338a27333b795d78
aminwhat <m.aminwhat@gmail.com>
2024-03-28 18:14:47 +0330

    seperate bootstrap and init

--- nokteh/src/main.ts
+++ nokteh/src/main.ts
@@ -1,8 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { bootstrap } from './bootstrap';
+import { init } from './init';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+init();
+
 bootstrap();
