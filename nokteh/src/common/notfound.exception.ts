650c6112e87388a00bcc2e8f8f33adbea4360f34
aminwhat <m.aminwhat@gmail.com>
2024-03-28 18:32:20 +0330

    add 404 html page and html to nokteh

---
+++ nokteh/src/common/notfound.exception.ts
@@ -0,0 +1,21 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  NotFoundException,
+} from '@nestjs/common';
+import { Response, Request } from 'express';
+import { join } from 'path';
+
+@Catch(NotFoundException)
+export class NotFoundExceptionFilter implements ExceptionFilter {
+  catch(exception: NotFoundException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request: Request = ctx.getRequest();
+
+    console.log({ request });
+
+    const response: Response = ctx.getResponse();
+    response.sendFile(join(__dirname, '..', 'static', 'index.html'));
+  }
+}
--- nokteh/src/main.ts
+++ nokteh/src/main.ts
@@ -1,6 +1,29 @@
-import { bootstrap } from './bootstrap';
-import { init } from './init';
+import * as dotenv from 'dotenv';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import * as clc from 'cli-color';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { NotFoundExceptionFilter } from './common/notfound.exception';
 
-init();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new NotFoundExceptionFilter());
+
+  app.use(helmet());
+  app.use(morgan('tiny'));
+  app.use(cookieParser());
+
+  const port: string = process.env.PORT;
+  await app.listen(port, () => {
+    console.log('\n' + clc.bgCyan('Running on port: ' + port) + '\n');
+  });
+}
+
+dotenv.config();
 
 bootstrap();
