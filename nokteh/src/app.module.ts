ca9b347bae49a8a9bb71aecd825ab420c96d50ab
aminwhat <m.aminwhat@gmail.com>
2024-04-12 18:45:56 +0330

    some improvements mostly to the nokteh

--- nokteh/src/app.module.ts
+++ nokteh/src/app.module.ts
@@ -3,12 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'static'),
     }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
   ],
   controllers: [AppController],
   providers: [AppService],
